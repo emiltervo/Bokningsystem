03215a0380594df46eea4abdd729721621a8570d
emiltervo <73493921+emiltervo@users.noreply.github.com>
2024-11-29 14:44:37 +0100

    Added model for Prescription that the Patient has.

--- src/main/java/models/Patient.java
+++ src/main/java/models/Patient.java
@@ -1,10 +1,16 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Patient extends User {
     private String journal;
-    public Patient(int userID, String name, String password, String email, String role, String journal){
+    private List<Prescription> prescriptions;
+
+    public Patient(int userID, String name, String password, String email, String role, String journal) {
         super(userID, name, password, email, role);
         this.journal = journal;
+        this.prescriptions = new ArrayList<>();
     }
 
     public String getJournal() {
@@ -13,5 +19,17 @@ public class Patient extends User {
 
     public void setJournal(String journal) {
         this.journal = journal;
+    }
+
+    public List<Prescription> getPrescriptions() {
+        return prescriptions;
+    }
+
+    public void addPrescription(Prescription prescription) {
+        this.prescriptions.add(prescription);
+    }
+
+    public void removePrescription(Prescription prescription) {
+        this.prescriptions.remove(prescription);
     }
 }
---
+++ src/main/java/models/Prescription.java
@@ -0,0 +1,59 @@
+package models;
+
+import java.util.Date;
+
+public class Prescription {
+    private String medicationName;
+    private String dosage;
+    private Date startDate;
+    private Date endDate;
+
+    public Prescription(String medicationName, String dosage, Date startDate, Date endDate) {
+        this.medicationName = medicationName;
+        this.dosage = dosage;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public String getMedicationName() {
+        return medicationName;
+    }
+
+    public void setMedicationName(String medicationName) {
+        this.medicationName = medicationName;
+    }
+
+    public String getDosage() {
+        return dosage;
+    }
+
+    public void setDosage(String dosage) {
+        this.dosage = dosage;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Prescription{" +
+                "medicationName='" + medicationName + '\'' +
+                ", dosage='" + dosage + '\'' +
+                ", startDate=" + startDate +
+                ", endDate=" + endDate +
+                '}';
+    }
+}
